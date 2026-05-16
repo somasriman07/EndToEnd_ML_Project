44e01ddcd498f309588214a7dd6621a77e8fe256
somasriman07 <somasriman3@gmail.com>
2026-05-16 22:56:53 +0530

    The Setup Commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+
+        # remove newline and spaces
+        requirements = [req.strip() for req in requirements]
+
+        # remove empty lines
+        requirements = [req for req in requirements if req]
+
+        # remove -e .
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="EndtoEndMLProject",
+    version="0.0.1",
+    author="Sriman Soma",
+    author_email="somasriman3@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
